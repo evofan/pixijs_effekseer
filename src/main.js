fb0e2f9d4a1f8406fb5ff79769e8ceb30be99d0b
evofan <evofan@outlook.jp>
2019-08-27 22:13:50 +0900

    fix particle effect

--- src/main.js
+++ src/main.js
@@ -1,18 +1,19 @@
-const WIDTH = 480;
-const HEIGHT = 320;
-const ASSET_BG = "images/bg_space.jpg";
+const WIDTH = 400;
+const HEIGHT = 600;
+const ASSET_BG = "images/pic_bg_space.jpg";
+const ASSET_START = "images/pic_start.png";
 
 // init
 let app = new PIXI.Application({
-	width: WIDTH,
-	height: HEIGHT
+  width: WIDTH,
+  height: HEIGHT
 });
 let canvas = document.getElementById("canvas");
 canvas.appendChild(app.view);
 app.renderer.backgroundColor = 0x000000;
 app.stage.interactive = true;
-let bg;
-let anim, anim2;
+let bg, start;
+let anim;
 let anim_speed = 0.5;
 
 let container_bg = new PIXI.Container();
@@ -21,8 +22,8 @@ container_bg.y = 0;
 app.stage.addChild(container_bg);
 
 let container = new PIXI.Container();
-container.width = 480;
-container.height = 480;
+container.width = 400;
+container.height = 600;
 container.x = 0;
 container.y = 0;
 container.pivot.x = 0;
@@ -31,9 +32,11 @@ container.interactive = true;
 app.stage.addChild(container);
 
 PIXI.loader
-	.add("bg_data", ASSET_BG)
-	.add("texture/test_sequence-1.json")
-	.load(onAssetsLoaded);
+  .add("bg_data", ASSET_BG)
+  // .add("texture/test_sequence-1.json")
+  .add("pic_start", ASSET_START)
+  .add("texture/temp3.json")
+  .load(onAssetsLoaded);
 
 /**
  * Asset load Complete
@@ -41,96 +44,75 @@ PIXI.loader
  * @param { object } res asset data
  */
 function onAssetsLoaded(loader, res) {
-	// BG
-	bg = new PIXI.Sprite(res.bg_data.texture);
-	container_bg.addChild(bg);
-	bg.x = 0;
-	bg.y = 0;
-	bg.interactive = true;
-	bg.on("tap", (event) => {
-		console.log("onTap"); // Desktop(Touch)
-	});
-	bg.on("click", (event) => {
-		console.log("click"); // Desktop
-	});
+  // BG
+  bg = new PIXI.Sprite(res.bg_data.texture);
+  container_bg.addChild(bg);
+  bg.x = 0;
+  bg.y = 0;
 
-	// Text
-	let text = new PIXI.Text("Touch the BG !", {
-		fontFamily: "Arial",
-		fontSize: 30,
-		fill: 0x666666,
-		align: "center",
-		fontWeight: "bold",
-		stroke: "#333333",
-		strokeThickness: 3,
-		dropShadow: false,
-		dropShadowColor: "#666666"
-	});
-	container.addChild(text);
-	text.x = 120;
-	text.y = 20;
+  // start point
+  start = new PIXI.Sprite(res.pic_start.texture);
+  container_bg.addChild(start);
+  start.x = 195;
+  start.y = 300;
+  start.width = 50;
+  start.height = 50;
+  start.pivot.x = 25;
+  start.pivot.y = 25;
 
-	// Particle animation
-	let frames = [];
-	let frames_length = 26;
-	for (let i = 1; i <= frames_length; i++) {
-		// if (i <= 9) {
-		// 	i = "0" + i;
-		// }
-		let id = PIXI.loader.resources["texture/test_sequence-1.json"].textures;
-		frames.push(PIXI.Texture.from(id[`test1_seq${i}.png`]));
-	}
+  // Text
+  let text = new PIXI.Text("Touch the BG !", {
+    fontFamily: "Arial",
+    fontSize: 30,
+    fill: 0x666666,
+    align: "center",
+    fontWeight: "bold"
+  });
+  container.addChild(text);
+  text.x = 90;
+  text.y = 40;
 
-	anim = new PIXI.extras.AnimatedSprite(frames); // ver.4 need .extra
-	anim.x = 263;
-	anim.y = 190;
-	anim.anchor.set(0.5);
-	anim.animationSpeed = 1;
-	anim.loop = false;
-	// anim.tint = 0x000000;
-	anim.visible = true;
-	anim.stop();
-	anim.onComplete = function() {
-		console.log("anim.totalFrames: ", anim.totalFrames);
-		console.log("animation end");
-		anim.interactive = true;
-	};
+  // Particle animation(sequence picture)
+  let frames = [];
+  let frames_length = 29;
+  for (let i = 0; i <= frames_length; i++) {
+    let id = PIXI.loader.resources["texture/temp3.json"].textures;
+    frames.push(PIXI.Texture.from(id[`test_seq.${i}.png`]));
+  }
 
-	anim.interactive = true;
-	anim.click = function() {
-		console.log("click cat");
-		anim.interactive = false;
-		anim.animationSpeed = anim_speed;
-		anim.gotoAndPlay(1);
-	};
-	anim.tap = function() {
-		console.log("touch cat");
-		anim.interactive = false;
-		anim.animationSpeed = anim_speed;
-		anim.gotoAndPlay(1);
-	};
+  anim = new PIXI.extras.AnimatedSprite(frames); // ver.4 need .extra
+  anim.x = 210;
+  anim.y = 300;
+  anim.anchor.set(0.5);
+  anim.animationSpeed = 1;
+  anim.loop = false;
+  // anim.tint = 0x000000;
+  anim.visible = true;
+  anim.stop();
+  anim.onComplete = function() {
+    console.log("anim.totalFrames: ", anim.totalFrames);
+    console.log("animation end\n\n");
+    anim.interactive = true;
+  };
 
-	container.addChild(anim);
+  anim.interactive = true;
+  anim.click = function() {
+    console.log("animation start(click)");
+    anim.interactive = false;
+    anim.animationSpeed = anim_speed;
+    anim.gotoAndPlay(1);
+  };
+  anim.tap = function() {
+    console.log("animation start(touch)");
+    anim.interactive = false;
+    anim.animationSpeed = anim_speed;
+    anim.gotoAndPlay(1);
+  };
 
-	// Enter Frame
-	app.ticker.add((delta) => {
-		//
-	});
-}
+  container.addChild(anim);
 
-function testAnimation(num) {
-	switch (num) {
-		case 1:
-			anim.animationSpeed = 0.1;
-			anim.gotoAndPlay(1);
-			break;
-
-		case 2:
-			anim.animationSpeed = anim_speed * 2;
-			anim.gotoAndPlay(1);
-			break;
-
-		default:
-			break;
-	}
+  // Enter Frame
+  app.ticker.add(delta => {
+    //
+  });
 }
