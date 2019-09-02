bd15c59fe9e1828285e958c983a3e01afa22ae4d
evofan <evofan@outlook.jp>
2019-09-02 23:02:03 +0900

    fix texture atras

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ const ASSET_START = "images/pic_start.png";
 
 // init
 let app = new PIXI.Application({
-  width: WIDTH,
-  height: HEIGHT
+	width: WIDTH,
+	height: HEIGHT
 });
 let canvas = document.getElementById("canvas");
 canvas.appendChild(app.view);
@@ -38,11 +38,12 @@ container.interactive = true;
 app.stage.addChild(container);
 
 PIXI.loader
-  .add("bg_data", ASSET_BG)
-  // .add("texture/test_sequence-1.json")
-  .add("pic_start", ASSET_START)
-  .add("texture/temp3.json")
-  .load(onAssetsLoaded);
+	.add("bg_data", ASSET_BG)
+	// .add("texture/test_sequence-1.json")
+	.add("pic_start", ASSET_START)
+	.add("texture/temp3-0.json")
+	.add("texture/temp3-1.json")
+	.load(onAssetsLoaded);
 
 /**
  * Asset load Complete
@@ -50,75 +51,76 @@ PIXI.loader
  * @param { object } res asset data
  */
 function onAssetsLoaded(loader, res) {
-  // BG
-  bg = new PIXI.Sprite(res.bg_data.texture);
-  container_bg.addChild(bg);
-  bg.x = 0;
-  bg.y = 0;
+	// BG
+	bg = new PIXI.Sprite(res.bg_data.texture);
+	container_bg.addChild(bg);
+	bg.x = 0;
+	bg.y = 0;
 
-  // start point
-  start = new PIXI.Sprite(res.pic_start.texture);
-  container_bg.addChild(start);
-  start.x = 195;
-  start.y = 300;
-  start.width = 50;
-  start.height = 50;
-  start.pivot.x = 25;
-  start.pivot.y = 25;
+	// start point
+	start = new PIXI.Sprite(res.pic_start.texture);
+	container_bg.addChild(start);
+	start.x = 195;
+	start.y = 300;
+	start.width = 50;
+	start.height = 50;
+	start.pivot.x = 25;
+	start.pivot.y = 25;
 
-  // Text
-  let text = new PIXI.Text("Touch the BG !", {
-    fontFamily: "Arial",
-    fontSize: 30,
-    fill: 0x666666,
-    align: "center",
-    fontWeight: "bold"
-  });
-  container.addChild(text);
-  text.x = 90;
-  text.y = 40;
+	// Text
+	let text = new PIXI.Text("Touch the BG !", {
+		fontFamily: "Arial",
+		fontSize: 30,
+		fill: 0x666666,
+		align: "center",
+		fontWeight: "bold"
+	});
+	container.addChild(text);
+	text.x = 90;
+	text.y = 40;
 
-  // Particle animation(sequence picture)
-  let frames = [];
-  let frames_length = 29;
-  for (let i = 0; i <= frames_length; i++) {
-    let id = PIXI.loader.resources["texture/temp3.json"].textures;
-    frames.push(PIXI.Texture.from(id[`test_seq.${i}.png`]));
-  }
+	// Particle animation(sequence picture)
+	let frames = [];
+	let frames_length = 29;
+	for (let i = 0; i <= frames_length; i++) {
+		let id = PIXI.loader.resources["texture/temp3-0.json"].textures;
+		frames.push(PIXI.Texture.from(id[`test_seq.${i}.png`]));
+	}
+	anim = new PIXI.extras.AnimatedSprite(frames); // ver.4 need .extra
+	anim.x = 210;
+	anim.y = 300;
+	anim.scale.x = 2;
+	anim.scale.y = 2;
+	anim.anchor.set(0.5);
+	anim.animationSpeed = 1;
+	anim.loop = false;
+	// anim.tint = 0x000000;
+	anim.visible = true;
+	anim.stop();
+	anim.onComplete = function() {
+		console.log("anim.totalFrames: ", anim.totalFrames);
+		console.log("animation end\n\n");
+		anim.interactive = true;
+	};
 
-  anim = new PIXI.extras.AnimatedSprite(frames); // ver.4 need .extra
-  anim.x = 210;
-  anim.y = 300;
-  anim.anchor.set(0.5);
-  anim.animationSpeed = 1;
-  anim.loop = false;
-  // anim.tint = 0x000000;
-  anim.visible = true;
-  anim.stop();
-  anim.onComplete = function() {
-    console.log("anim.totalFrames: ", anim.totalFrames);
-    console.log("animation end\n\n");
-    anim.interactive = true;
-  };
+	anim.interactive = true;
+	anim.click = function() {
+		console.log("animation start(click)");
+		anim.interactive = false;
+		anim.animationSpeed = anim_speed;
+		anim.gotoAndPlay(1);
+	};
+	anim.tap = function() {
+		console.log("animation start(touch)");
+		anim.interactive = false;
+		anim.animationSpeed = anim_speed;
+		anim.gotoAndPlay(1);
+	};
 
-  anim.interactive = true;
-  anim.click = function() {
-    console.log("animation start(click)");
-    anim.interactive = false;
-    anim.animationSpeed = anim_speed;
-    anim.gotoAndPlay(1);
-  };
-  anim.tap = function() {
-    console.log("animation start(touch)");
-    anim.interactive = false;
-    anim.animationSpeed = anim_speed;
-    anim.gotoAndPlay(1);
-  };
+	container.addChild(anim);
 
-  container.addChild(anim);
-
-  // Enter Frame
-  app.ticker.add(delta => {
-    stats.update();
-  });
+	// Enter Frame
+	app.ticker.add((delta) => {
+		stats.update();
+	});
 }
